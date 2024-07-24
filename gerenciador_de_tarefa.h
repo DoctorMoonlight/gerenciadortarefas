891816240595238ecce82cb3fb2f61a790b569e3
Maria C. Moreira <91071877+DoctorMoonlight@users.noreply.github.com>
2024-07-24 19:48:24 -0300

    Update gerenciador_de_tarefa.h

--- gerenciador_de_tarefa.h
+++ gerenciador_de_tarefa.h
@@ -1,1 +1,25 @@
+#ifndef GERENCIADOR_H
+#define GERENCIADOR_H
 
+#include <vector>
+#include "tarefa.h"
+#include "notificacao.h"
+#include "etiqueta.h"
+#include "historico.h"
+
+class GerenciadorDeTarefas {
+public:
+    void adicionarTarefa(const Tarefa& tarefa);
+    void eliminarTarefa(const std::string& descricao);
+    void editarTarefa(const std::string& descricao, const Tarefa& tarefaAtualizada);
+    void exibirTarefas() const;
+    void marcarComoConcluida(const std::string& descricao);
+    void gerenciarEtiquetas(const std::string& descricao, const Etiqueta& etiqueta);
+    void exibirHistorico() const;
+
+private:
+    std::vector<Tarefa> tarefas;
+    Historico historico;
+};
+
+#endif
