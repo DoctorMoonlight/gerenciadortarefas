ae62ed61b30c5effe1ece5d8f941df2707aaa707
Bruna Kamily <brunakamilydossantos@gmail.com>
2024-07-24 19:52:48 -0300

    Update historico.h

--- historico.h
+++ historico.h
@@ -1,1 +1,14 @@
+#ifndef HISTORICO_H
+#define HISTORICO_H
 
+#include <vector>
+#include "tarefa.h"
+
+class Historico {
+public:
+    void adicionarTarefaConcluida(const Tarefa& tarefa);
+    void exibirHistorico() const;
+
+private:
+    std::vector<Tarefa> tarefasConcluidas;};
+#endif 
