a50479665f3a14d7106d83d26f2ddbe7f7942a8b
anasarahsg <165835956+anasarahsg@users.noreply.github.com>
2024-07-24 19:52:51 -0300

    Update etiqueta.h

--- etiqueta.h
+++ etiqueta.h
@@ -1,1 +1,19 @@
+#ifndef ETIQUETA_H
+#define ETIQUETA_H
 
+#include <string>
+
+class Etiqueta {
+public:
+    Etiqueta(const std::string& nome, int prioridade = 0);
+    void setNome(const std::string& nome);
+    std::string getNome() const;
+    void setPrioridade(int prioridade);
+    int getPrioridade() const;
+
+private:
+    std::string nome;
+    int prioridade;
+};
+
+#endif // ETIQUETA_H
