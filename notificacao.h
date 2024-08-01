9a25e39b77563e84738b22a14969cf42076c60c0
Elen <elenufmg@gmail.com>
2024-08-01 20:10:03 -0300

    Update notificacao.h

--- notificacao.h
+++ notificacao.h
@@ -1,40 +1,41 @@
 #ifndef NOTIFICACAO_H
-
 #define NOTIFICACAO_H
-
-#include <string> // Inclui a biblioteca de string do C++
-
+#include <string>
+#include <chrono>
+#include <ctime>
 class Notificacao {
 public:
-    // Construtor que inicializa uma notificação com mensagem, data e horário
-    Notificacao(const std::string& mensagem, const std::string& data, const std::string& horario);
-    
-    // Setter para a mensagem da notificação
-    void setMensagem(const std::string& mensagem);
-    // Getter para obter a mensagem da notificação
-    std::string getMensagem() const;
-    
-    // Setter para a data da notificação
-    void setData(const std::string& data);
-    // Getter para obter a data da notificação
-    std::string getData() const;
-    
-    // Setter para o horário da notificação
-    void setHorario(const std::string& horario);
-    // Getter para obter o horário da notificação
-    std::string getHorario() const;
-    
-    // Função para notificar (possivelmente emitir uma notificação ou imprimir algo)
-    void notificar() const;
+ // Construtor padrão
+ Notificacao();
 
+ // Construtor que inicializa uma notificação com mensagem, data e horário
+ Notificacao(const std::string& mensagem, const std::string& data, const std::string&
+horario);
+
+ // Setter para a mensagem da notificação
+ void setMensagem(const std::string& mensagem);
+ // Getter para obter a mensagem da notificação
+ std::string getMensagem() const;
+
+ // Setter para a data da notificação
+ void setData(const std::string& data);
+ // Getter para obter a data da notificação
+ std::string getData() const;
+// Setter para o horário da notificação
+ void setHorario(const std::string& horario);
+ // Getter para obter o horário da notificação
+ std::string getHorario() const;
+
+ // Função para notificar (emitir um som e imprimir algo)
+ void notificar() const;
+
+ // Função para verificar se a notificação deve ser exibida
+ bool verificarHoraNotificacao() const;
 private:
-    // Atributo que armazena a mensagem da notificação
-    std::string mensagem;
-    // Atributo que armazena a data da notificação
-    std::string data;
-    // Atributo que armazena o horário da notificação
-    std::string horario;
+ std::string mensagem;
+ std::string data;
+ std::string horario;
+// Converte uma string de data e hora para std::tm
+ std::tm stringParaTm(const std::string& data, const std::string& horario) const;
 };
-
-
 #endif // NOTIFICACAO_H
