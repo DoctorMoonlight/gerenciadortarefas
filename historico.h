e1c6df018403853eef3763634e8c3c3e51ec5c64
Bruna Kamily <brunakamilydossantos@gmail.com>
2024-08-01 16:55:19 -0300

    Update historico.h

--- historico.h
+++ historico.h
@@ -1,16 +1,19 @@
 #ifndef HISTORICO_H
 #define HISTORICO_H
 
-#include "tarefa.h"//Inclui o arquivo de cabeçalho para definir a classe Tarefa
-#include <vector>//Inclui a biblioteca padrão vector do C++
+#include "tarefa.h" // Inclui o arquivo de cabeçalho pra definir a classe Tarefa
+#include <vector> // Inclui a biblioteca padrão vector do C++
 
-//Inicialização da classe historico
+using namespace std; // Adiciona o namespace std
+
+// Inicialização da classe Historico
 class Historico {
 public:
-    void adicionarTarefaConcluida(const Tarefa& tarefa);//Adiciona uma tarefa concluída ao histórico
-    void exibirHistorico() const;//Exibe o histórico das tarefas concluídas
+    void adicionarTarefaConcluida(const Tarefa& tarefa); // Adiciona uma tarefa concluída ao histórico
+    void exibirHistorico() const; // Exibe o histórico das tarefas concluídas
 
 private:
-    std::vector<Tarefa> tarefasConcluidas;//Vetor privado que armazena as tarefas concluídas
+    vector<Tarefa> tarefasConcluidas; // Vetor privado que armazena as tarefas concluídas
 };
-#endif //Encerra a prevenção de inclusão múltipla
+
+#endif // Encerra a prevenção de inclusão múltipla
