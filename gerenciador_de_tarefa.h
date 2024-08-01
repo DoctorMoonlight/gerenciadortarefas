9ea6256c4d8de2aaa8447fc3cd456d568506079b
Maria C. Moreira <91071877+DoctorMoonlight@users.noreply.github.com>
2024-08-01 18:30:14 -0300

    Update gerenciador_de_tarefa.h

--- gerenciador_de_tarefa.h
+++ gerenciador_de_tarefa.h
@@ -1,31 +1,32 @@
 #ifndef GERENCIADOR_H
 #define GERENCIADOR_H
-// Incluindo as bibliotecas necessárias
-#include <vector>           // Biblioteca padrão para utilização de vetores
-#include "tarefa.h"         // Inclusão do cabeçalho da classe Tarefa
-#include "notificacao.h"    // Inclusão do cabeçalho da classe Notificação
-#include "etiqueta.h"       // Inclusão do cabeçalho da classe Etiqueta
-#include "historico.h"      // Inclusão do cabeçalho da classe Historico
-
+#include <vector>
+#include "tarefa.h"
+#include "notificacao.h"
+#include "etiqueta.h"
+#include "historico.h"
+using namespace std;
 // Declaração da classe GerenciadorDeTarefas
 class GerenciadorDeTarefas {
 public:
-    // Método para adicionar uma nova tarefa
-    void adicionarTarefa(const Tarefa& tarefa);
-    // Método para eliminar uma tarefa baseada na descrição
-    void eliminarTarefa(const std::string& descricao);
-    // Método para editar uma tarefa existente
-    void editarTarefa(const std::string& descricao, const Tarefa& tarefaAtualizada);
-    // Método para exibir todas as tarefas
-    void exibirTarefas() const;
-    // Método para marcar uma tarefa como concluída
-    void marcarComoConcluida(const std::string& descricao);
-    // Método para gerenciar etiquetas de uma tarefa específica
-    void gerenciarEtiquetas(const std::string& descricao, const Etiqueta& etiqueta);
-    // Método para exibir o histórico de tarefas
-    void exibirHistorico() const;
+ // Método para adicionar uma nova tarefa
+ void adicionarTarefa(const Tarefa& tarefa);
+ // Método para eliminar uma tarefa baseada na descrição
+ void eliminarTarefa(const string& descricao);
+ // Método para editar uma tarefa existente
+ void editarTarefa(const string& descricao, const Tarefa& tarefaAtualizada);
+ // Método para exibir todas as tarefas
+ void exibirTarefas() const;
+ // Método para marcar uma tarefa como concluída
+ void marcarComoConcluida(const string& descricao);
+ // Método para gerenciar etiquetas de uma tarefa específica
+ void gerenciarEtiquetas(const string& descricao, const Etiqueta& etiqueta);
+ // Método para exibir o histórico de tarefas
+ void exibirHistorico() const;
+ // Método para ordenar as tarefas por prioridade
+ void ordenarPorPrioridade();
 private:
-    std::vector<Tarefa> tarefas;  // Vetor que armazena as tarefas
-    Historico historico;          // Objeto que armazena o histórico das tarefas
+ vector<Tarefa> tarefas; // Vetor que armazena as tarefas
+ Historico historico; // Objeto que armazena o histórico das tarefas
 };
 #endif // GERENCIADOR_H
