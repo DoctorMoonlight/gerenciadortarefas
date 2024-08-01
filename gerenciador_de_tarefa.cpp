02b6a9cbb14604c8aa44800d8fbbc0d0eaccf73c
Maria C. Moreira <91071877+DoctorMoonlight@users.noreply.github.com>
2024-08-01 18:29:39 -0300

    Update gerenciador_de_tarefa.cpp

--- gerenciador_de_tarefa.cpp
+++ gerenciador_de_tarefa.cpp
@@ -1,88 +1,72 @@
-#define CATCH_CONFIG_MAIN
-#include <iostream>
-#include "catch_amalgamated.hpp"
 #include "gerenciador_de_tarefa.h"
-#include "tarefa.h"
-#include "etiqueta.h"
-#include "notificacao.h"
-
-// Teste para adicionar e eliminar tarefa
-TEST_CASE("Adicionar e Eliminar Tarefa") {
-    GerenciadorDeTarefas g;
-
-    Etiqueta etiqueta("Importante", 3);
-    Notificacao notificacao("Reunião", "28/07/2024", "10:00");
-    Tarefa t("Tarefa 1", "28/07/2024", "09:00", etiqueta, notificacao);
-
-    g.adicionarTarefa(t);
-    REQUIRE(g.getTarefas().size() == 1);
-
-    g.eliminarTarefa("Tarefa 1");
-    REQUIRE(g.getTarefas().size() == 0);
+#include <iostream>
+#include <algorithm>
+using namespace std;
+// Método para adicionar uma nova tarefa
+void GerenciadorDeTarefas::adicionarTarefa(const Tarefa& tarefa) {
+ tarefas.push_back(tarefa);
 }
-
-// Teste para editar tarefa
-TEST_CASE("Editar Tarefa") {
-    GerenciadorDeTarefas g;
-
-    Etiqueta etiqueta1("Urgente", 5);
-    Notificacao notificacao1("Notificar", "28/07/2024", "10:00");
-    Tarefa t1("Tarefa 1", "28/07/2024", "09:00", etiqueta1, notificacao1);
-
-    Etiqueta etiqueta2("Importante", 2);
-    Notificacao notificacao2("Atualizar", "29/07/2024", "11:00");
-    Tarefa t2("Tarefa 1", "29/07/2024", "10:00", etiqueta2, notificacao2);
-
-    g.adicionarTarefa(t1);
-    g.editarTarefa("Tarefa 1", t2);
-
-    REQUIRE(g.getTarefas().size() == 1);
-    REQUIRE(g.getTarefas()[0].getDescricao() == "Tarefa 1");
-    REQUIRE(g.getTarefas()[0].getData() == "29/07/2024");
-    REQUIRE(g.getTarefas()[0].getHorario() == "10:00");
-    REQUIRE(g.getTarefas()[0].getEtiqueta().getPrioridade() == 2);
+// Método para eliminar uma tarefa baseada na descrição
+void GerenciadorDeTarefas::eliminarTarefa(const string& descricao) {
+ tarefas.erase(
+ remove_if(tarefas.begin(), tarefas.end(), [&descricao](const Tarefa& tarefa) {
+ return tarefa.getDescricao() == descricao;
+ }),
+ tarefas.end());
 }
-
-// Teste para ordenar tarefas por prioridade
-TEST_CASE("Ordenar Tarefas por Prioridade") {
-    GerenciadorDeTarefas g;
-
-    Etiqueta etiqueta1("Baixa", 1);
-    Etiqueta etiqueta2("Alta", 5);
-    Notificacao notificacao("Notificar", "28/07/2024", "10:00");
-    Tarefa t1("Tarefa 1", "28/07/2024", "09:00", etiqueta1, notificacao);
-    Tarefa t2("Tarefa 2", "28/07/2024", "10:00", etiqueta2, notificacao);
-
-    g.adicionarTarefa(t1);
-    g.adicionarTarefa(t2);
-    g.ordenarPorPrioridade();
-
-    REQUIRE(g.getTarefas()[0].getEtiqueta().getPrioridade() == 5);
-    REQUIRE(g.getTarefas()[1].getEtiqueta().getPrioridade() == 1);
+// Método para editar uma tarefa existente
+void GerenciadorDeTarefas::editarTarefa(const string& descricao, const Tarefa&
+tarefaAtualizada) {
+ for (auto& tarefa : tarefas) {
+ if (tarefa.getDescricao() == descricao) {
+ tarefa = tarefaAtualizada;
+ break;
+ }
+ }
 }
-
-// Teste para marcar tarefa como concluída e exibir histórico
-TEST_CASE("Marcar Tarefa como Concluída e Exibir Histórico") {
-    GerenciadorDeTarefas g;
-
-    Etiqueta etiqueta("Importante", 2);
-    Notificacao notificacao("Notificar", "28/07/2024", "10:00");
-    Tarefa t("Tarefa 1", "28/07/2024", "09:00", etiqueta, notificacao);
-
-    g.adicionarTarefa(t);
-    g.marcarComoConcluida("Tarefa 1");
-
-    // Verificar se o histórico foi atualizado
-    std::ostringstream oss;
-    {
-        std::streambuf* p_cout_streambuf = std::cout.rdbuf();
-        std::cout.rdbuf(oss.rdbuf());
-
-        g.exibirHistorico();
-        
-        std::cout.rdbuf(p_cout_streambuf);
-    }
-    
-    std::string output = oss.str();
-    REQUIRE(output == "Tarefa: Tarefa 1, Data: 28/07/2024, Horário: 09:00\n");
+// Método para ordenar taredas por prioridade
+void GerenciadorDeTarefas::ordenarPorPrioridade() {
+ sort(tarefas.begin(), tarefas.end(), [](const Tarefa& a, const Tarefa& b) {
+ return a.getEtiqueta().getPrioridade() > b.getEtiqueta().getPrioridade();
+ });
+}
+// Método para exibir todas as tarefas
+void GerenciadorDeTarefas::exibirTarefas() const {
+ // Primeiro, ordena as tarefas por prioridade
+ vector<Tarefa> tarefasOrdenadas = tarefas;
+ sort(tarefasOrdenadas.begin(), tarefasOrdenadas.end(), [](const Tarefa& a, const
+Tarefa& b) {
+ return a.getEtiqueta().getPrioridade() > b.getEtiqueta().getPrioridade();
+ });
+ // Em seguida, exibe as tarefas ordenadas
+ for (const auto& tarefa : tarefasOrdenadas) {
+ cout << "Tarefa: " << tarefa.getDescricao()
+ << ", Data: " << tarefa.getData()
+ << ", Horário: " << tarefa.getHorario()
+ << ", Prioridade: " << tarefa.getEtiqueta().getPrioridade() << endl;
+ }
+}
+// Método para marcar uma tarefa como concluída
+void GerenciadorDeTarefas::marcarComoConcluida(const string& descricao) {
+ for (auto& tarefa : tarefas) {
+ if (tarefa.getDescricao() == descricao) {
+ tarefa.setStatus(true);
+ historico.adicionarTarefaConcluida(tarefa);
+ break;
+ }
+ }
+}
+// Método para gerenciar etiquetas de uma tarefa específica
+void GerenciadorDeTarefas::gerenciarEtiquetas(const string& descricao, const
+Etiqueta& etiqueta) {
+ for (auto& tarefa : tarefas) {
+ if (tarefa.getDescricao() == descricao) {
+ tarefa.setEtiqueta(etiqueta);
+ break;
+ }
+ }
+}
+// Método para exibir o histórico de tarefas
+void GerenciadorDeTarefas::exibirHistorico() const {
+ historico.exibirHistorico();
 }
