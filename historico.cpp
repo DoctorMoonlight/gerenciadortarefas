1ba55f9f37c15f1aba56b05d2937ea922ac0f9fe
Bruna Kamily <brunakamilydossantos@gmail.com>
2024-08-01 16:59:03 -0300

    Update historico.cpp

--- historico.cpp
+++ historico.cpp
@@ -1,14 +1,15 @@
-#include "historico.h" 
+#include "historico.h"  
 #include <iostream> 
+using namespace std; 
 
-// Adiciona uma tarefa concluída ao histórico 
-void Historico::adicionarTarefaConcluida(const Tarefa& tarefa) { 
-    tarefasConcluidas.push_back(tarefa); 
-}
+// Adiciona uma tarefa concluída ao histórico  
+void Historico::adicionarTarefaConcluida(const Tarefa& tarefa) {  
+    tarefasConcluidas.push_back(tarefa); 
+}  
 
-// Exibe o histórico das tarefas concluídas 
-void Historico::exibirHistorico() const { 
-    for (const auto& tarefa : tarefasConcluidas) { 
-        std::cout << "Tarefa: " << tarefa.getDescricao() << ", Data: " << tarefa.getData() << ", Horário: " << tarefa.getHorario() << std::endl; 
-    } 
+// Exibe o histórico das tarefas concluídas  
+void Historico::exibirHistorico() const {  
+    for (const auto& tarefa : tarefasConcluidas) {  
+        cout << "Tarefa: " << tarefa.getDescricao() << ", Data: " << tarefa.getData() << ", Horário: " << tarefa.getHorario() << endl;  
+    }  
 } 
