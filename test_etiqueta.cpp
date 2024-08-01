d35cbf50543bd05f60d547f8d83f45f0f4af0824
Bruna Kamily <brunakamilydossantos@gmail.com>
2024-08-01 17:02:10 -0300

    Create test_etiqueta.cpp

---
+++ test_etiqueta.cpp
@@ -0,0 +1,31 @@
+#define CATCH_CONFIG_MAIN
+#include "catch_amalgamated.hpp"
+#include "etiqueta.h"
+
+// Teste para o construtor padrão
+TEST_CASE("Construtor Padrão da Etiqueta") {
+Etiqueta e;
+REQUIRE(e.getNome() == "");
+REQUIRE(e.getPrioridade() == 0);
+}
+
+// Teste para o construtor parametrizado
+TEST_CASE("Construtor Parametrizado da Etiqueta") {
+Etiqueta e("Urgente", 5);
+REQUIRE(e.getNome() == "Urgente");
+REQUIRE(e.getPrioridade() == 5);
+}
+
+// Teste para setNome e getNome
+TEST_CASE("Set e Get Nome") {
+Etiqueta e;
+e.setNome("Importante");
+REQUIRE(e.getNome() == "Importante");
+}
+
+// Teste para setPrioridade e getPrioridade
+TEST_CASE("Set e Get Prioridade") {
+Etiqueta e;
+e.setPrioridade(10);
+REQUIRE(e.getPrioridade() == 10);
+}
