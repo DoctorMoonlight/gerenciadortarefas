557b3b95dcb147483003ac13aca37ec5c8b71061
Bruna Kamily <brunakamilydossantos@gmail.com>
2024-08-06 19:57:48 -0300

    Update test_etiqueta.cpp

--- test_etiqueta.cpp
+++ test_etiqueta.cpp
@@ -2,30 +2,36 @@
 #include "catch_amalgamated.hpp"
 #include "etiqueta.h"
 
+using namespace std;
+
 // Teste para o construtor padrão
-TEST_CASE("Construtor Padrão da Etiqueta") {
-Etiqueta e;
-REQUIRE(e.getNome() == "");
-REQUIRE(e.getPrioridade() == 0);
+TEST_CASE("Construtor Padrão da Etiqueta") 
+{
+    Etiqueta e;
+    REQUIRE(e.getNome() == "");
+    REQUIRE(e.getPrioridade() == "");
 }
 
 // Teste para o construtor parametrizado
-TEST_CASE("Construtor Parametrizado da Etiqueta") {
-Etiqueta e("Urgente", 5);
-REQUIRE(e.getNome() == "Urgente");
-REQUIRE(e.getPrioridade() == 5);
+TEST_CASE("Construtor Parametrizado da Etiqueta") 
+{
+    Etiqueta e("Urgente", "5");
+    REQUIRE(e.getNome() == "Urgente");
+    REQUIRE(e.getPrioridade() == "5");
 }
 
 // Teste para setNome e getNome
-TEST_CASE("Set e Get Nome") {
-Etiqueta e;
-e.setNome("Importante");
-REQUIRE(e.getNome() == "Importante");
+TEST_CASE("Set e Get Nome") 
+{
+    Etiqueta e;
+    e.setNome("Importante");
+    REQUIRE(e.getNome() == "Importante");
 }
 
 // Teste para setPrioridade e getPrioridade
-TEST_CASE("Set e Get Prioridade") {
-Etiqueta e;
-e.setPrioridade(10);
-REQUIRE(e.getPrioridade() == 10);
+TEST_CASE("Set e Get Prioridade") 
+{
+    Etiqueta e;
+    e.setPrioridade("5");
+    REQUIRE(e.getPrioridade() == "5");
 }
