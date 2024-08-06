8614a62854e708cbafc3bf576aa128a7cd83c77d
anasarahsg <165835956+anasarahsg@users.noreply.github.com>
2024-08-06 19:55:07 -0300

    Update etiqueta.cpp

--- etiqueta.cpp
+++ etiqueta.cpp
@@ -1,21 +1,32 @@
 #include "etiqueta.h"
-// Construtor que aceita um nome e uma prioridade com valor padrão 0
-Etiqueta::Etiqueta(const string& nome, int prioridade)
- : nome(nome), prioridade(prioridade) {}
+
+using namespace std;
+
+// Construtor que aceita um nome e uma prioridade 
+Etiqueta::Etiqueta(const string& nome, const string& prioridade)
+    : nome(nome), prioridade(prioridade) {}
+
 // Define o nome da etiqueta
-void Etiqueta::setNome(const string& nome) {
- this->nome = nome;
+void Etiqueta::setNome(const string& nome) 
+{
+    this->nome = nome;
 }
+
 // Retorna o nome da etiqueta
-string Etiqueta::getNome() const {
- return nome;
+string Etiqueta::getNome() const 
+{
+    return nome;
 }
+
 // Define a prioridade da etiqueta
-void Etiqueta::setPrioridade(int prioridade) {
- this->prioridade = prioridade;
+void Etiqueta::setPrioridade(const string& prioridade) 
+{
+    this->prioridade = prioridade;
 }
+
 // Retorna a prioridade da etiqueta
-int Etiqueta::getPrioridade() const {
- return prioridade;
+string Etiqueta::getPrioridade() const 
+{
+    return prioridade;
 }
 
