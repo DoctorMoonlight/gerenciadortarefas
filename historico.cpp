3ec8777a004f4165c31a8f9f6cec5523b4d4f036
Bruna Kamily <brunakamilydossantos@gmail.com>
2024-08-06 19:52:30 -0300

    Update historico.cpp

--- historico.cpp
+++ historico.cpp
@@ -1,15 +1,30 @@
-#include "historico.h"  
-#include <iostream> 
-using namespace std; 
+#include "historico.h"
+#include <iostream>
 
-// Adiciona uma tarefa concluída ao histórico  
-void Historico::adicionarTarefaConcluida(const Tarefa& tarefa) {  
-    tarefasConcluidas.push_back(tarefa); 
-}  
+using namespace std;
 
-// Exibe o histórico das tarefas concluídas  
-void Historico::exibirHistorico() const {  
-    for (const auto& tarefa : tarefasConcluidas) {  
-        cout << "Tarefa: " << tarefa.getDescricao() << ", Data: " << tarefa.getData() << ", Horário: " << tarefa.getHorario() << endl;  
-    }  
-} 
+// Adiciona uma tarefa concluída ao histórico
+void Historico::adicionarTarefaConcluida(const Tarefa& tarefa) 
+{
+    tarefasConcluidas.push_back(tarefa);
+}
+
+// Exibe o histórico das tarefas concluídas
+void Historico::exibirHistorico() const 
+{
+    // Se o vetor de tarefas concluídas estiver não estiver vazio, exibe a lista na tela
+    if(!tarefasConcluidas.empty())
+    {
+        cout << "\nAs tarefas concluídas são:\n";
+        for (const auto& tarefa : tarefasConcluidas) 
+        {
+            cout << "Tarefa: " << tarefa.getDescricao() << ", Data: " << tarefa.getData() << ", Horário: " << tarefa.getHorario() << endl << endl;
+        }
+    }
+
+    // Se não, exibe uma mensagem informando que não há tarefas concluídas
+    else
+    {
+        cout << "\nNão há nenhuma tarefa concluída a ser exibida.\n\n";
+    }
+}
